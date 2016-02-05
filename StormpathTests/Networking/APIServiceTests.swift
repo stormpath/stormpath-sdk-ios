309400eedaaa41753b3d28ed0d138044183c5f94
Edward Jiang <edward@stormpath.com>
2016-02-05 11:11:02 -0800

    updated tests to be consistent with new syntax

--- StormpathTests/Networking/APIServiceTests.swift
+++ StormpathTests/Networking/APIServiceTests.swift
@@ -10,6 +10,7 @@ import XCTest
 @testable import Stormpath
 
 class APIServiceTests: XCTestCase {
+    var stormpath = Stormpath.sharedSession
     
     override func setUp() {
         super.setUp()
@@ -18,7 +19,6 @@ class APIServiceTests: XCTestCase {
     override func tearDown() {
         super.tearDown()
         
-        Stormpath.APIURL = nil
         KeychainService.accessToken = nil
         KeychainService.refreshToken = nil
     }
@@ -27,16 +27,14 @@ class APIServiceTests: XCTestCase {
     
     // This will test that the API exists at the URL used for testing
     func testAPIExistsAtGivenURL() {
-        Stormpath.setUpWithURL(APIURL)
-        XCTAssertNotNil(Stormpath.APIURL)
         
-        let URL = NSURL(string: Stormpath.APIURL!)
+        let URL = stormpath.configuration.APIURL
         XCTAssertNotNil(URL)
         
         let expectation = expectationWithDescription("GET \(URL)")
         
         let session = NSURLSession.sharedSession()
-        let task = session.dataTaskWithURL(URL!) { data, response, error in
+        let task = session.dataTaskWithURL(URL) { data, response, error in
             XCTAssertNotNil(data, "data should not be nil")
             XCTAssertNil(error, "error should be nil")
             
@@ -62,16 +60,14 @@ class APIServiceTests: XCTestCase {
     
     // Same as above, but this one makes sure the trailing slash doesn't create odd strings
     func testAPIExistsAtGivenURLWithTrailingSlash() {
-        Stormpath.setUpWithURL(APIURL)
-        XCTAssertNotNil(Stormpath.APIURL)
         
-        let URL = NSURL(string: Stormpath.APIURL!)
+        let URL = stormpath.configuration.APIURL
         XCTAssertNotNil(URL)
         
         let expectation = expectationWithDescription("GET \(URL)")
         
         let session = NSURLSession.sharedSession()
-        let task = session.dataTaskWithURL(URL!) { data, response, error in
+        let task = session.dataTaskWithURL(URL) { data, response, error in
             XCTAssertNotNil(data, "data should not be nil")
             XCTAssertNil(error, "error should be nil")
             
@@ -99,7 +95,6 @@ class APIServiceTests: XCTestCase {
     
     // Test that the valid response produces a valid dictionary
     func testValidRegisterResponseParsing() {
-        Stormpath.setUpWithURL(APIURL)
         
         let validResponse: String = "{\"email\":\"user@stormpath.com\",\"password\":\"Password1\",\"username\":\"user@stormpath.com\"}"
         let validData: NSData = validResponse.dataUsingEncoding(NSUTF8StringEncoding)!
@@ -112,7 +107,6 @@ class APIServiceTests: XCTestCase {
     
     // Invalid response should not produce a dictionary but an error
     func testInvalidRegisterResponseParsing() {
-        Stormpath.setUpWithURL(APIURL)
         
         let invalidResponse = "this_should_not_work"
         let invalidData = invalidResponse.dataUsingEncoding(NSUTF8StringEncoding)!
@@ -125,7 +119,6 @@ class APIServiceTests: XCTestCase {
     
     // This one tests no data given in a response
     func testErrorRegisterResponseParsing() {
-        Stormpath.setUpWithURL(APIURL)
         
         APIService.parseDictionaryResponseData(nil) { (userDictionary, error) -> Void in
             XCTAssertNil(userDictionary)
@@ -139,7 +132,7 @@ class APIServiceTests: XCTestCase {
         KeychainService.accessToken = nil
         KeychainService.refreshToken = nil
 
-        let response: NSHTTPURLResponse = NSHTTPURLResponse.init(URL: NSURL.init(string: APIURL)!, statusCode: 200, HTTPVersion: "", headerFields: ["":""])!
+        let response: NSHTTPURLResponse = NSHTTPURLResponse.init(URL: StormpathConfiguration.defaultConfiguration.APIURL, statusCode: 200, HTTPVersion: "", headerFields: ["":""])!
         APIService.parseRegisterHeaderData(response)
         
         // Invalid headers should not parse anything
@@ -151,7 +144,6 @@ class APIServiceTests: XCTestCase {
     
     // Test that the valid login response returns the access_token, and stores both access_token and refresh_token properly
     func testValidLoginResponseParsing() {
-        Stormpath.setUpWithURL(APIURL)
         
         let validResponse: String = "{\"access_token\":\"accessToken\",\"refresh_token\":\"refreshToken\",\"token_type\":\"Bearer\",\"expires_in\":3600,\"stormpath_access_token_href\":\"https://api.stormpath.com/v1/accessTokens/tokens\"}"
         let validData: NSData = validResponse.dataUsingEncoding(NSUTF8StringEncoding)!
@@ -164,14 +156,13 @@ class APIServiceTests: XCTestCase {
             
             // Confirm that values are stored in Keychain properly
             XCTAssertEqual("accessToken", KeychainService.accessToken)
-            XCTAssertEqual(KeychainService.accessToken, Stormpath.accessToken)
+            XCTAssertEqual(KeychainService.accessToken, self.stormpath.accessToken)
             XCTAssertEqual("refreshToken", KeychainService.refreshToken)
         }
     }
     
     // Invalid JSON should not store either access_token or refresh_token
     func testInvalidLoginResponseParsing() {
-        Stormpath.setUpWithURL(APIURL)
         
         let invalidResponse = "this_should_not_work"
         let invalidData = invalidResponse.dataUsingEncoding(NSUTF8StringEncoding)!
@@ -179,31 +170,29 @@ class APIServiceTests: XCTestCase {
         APIService.parseLoginResponseData(invalidData) { (accesToken, error) -> Void in
             XCTAssertNil(accesToken)
             XCTAssertNil(KeychainService.accessToken)
-            XCTAssertNil(Stormpath.accessToken)
+            XCTAssertNil(self.stormpath.accessToken)
         }
     }
     
     // No data received should not store tokens either
     func testErrorLoginResponseParsing() {
-        Stormpath.setUpWithURL(APIURL)
                 
         APIService.parseLoginResponseData(nil) { (accessToken, error) -> Void in
             XCTAssertNil(accessToken)
             XCTAssertNil(KeychainService.accessToken)
-            XCTAssertNil(Stormpath.accessToken)
+            XCTAssertNil(self.stormpath.accessToken)
             XCTAssertNotNil(error)
         }
     }
     
     // Test that refreshing access_tokens fails when the refresh token is not set
     func testRefreshAccessTokenWhenRefreshTokenMising() {
-        Stormpath.setUpWithURL(APIURL)
         KeychainService.refreshToken = nil
         
-        APIService.refreshAccessToken(nil) { (accessToken, error) -> Void in
+        stormpath.apiService.refreshAccessToken() { (accessToken, error) -> Void in
             XCTAssertNil(accessToken)
             XCTAssertNil(KeychainService.accessToken)
-            XCTAssertNil(Stormpath.accessToken)
+            XCTAssertNil(Stormpath.sharedSession.accessToken)
             XCTAssertNotNil(error)
         }
     }
