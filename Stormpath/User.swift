d4c2dcb5c403c91869d9349c090f10c1c2f1bb02
Edward Jiang <edward@stormpath.com>
2016-02-12 11:48:28 -0800

    allows user to init from JSON

--- Stormpath/User.swift
+++ Stormpath/User.swift
@@ -38,10 +38,34 @@ public class User: NSObject {
         self.customData = customData
     }
     
-//    init?(fromJSON jsonData: NSData) {
-//        let json = NSJSONSerialization.JSONObjectWithData(jsonData, options: [])
-//        
-//    }
+    init?(fromJSON jsonData: NSData) {
+        super.init()
+        guard let json = try? NSJSONSerialization.JSONObjectWithData(jsonData, options: []),
+            hrefString = json["href"] as? String,
+            href = NSURL(string: hrefString),
+            username = json["username"] as? String,
+            email = json["email"] as? String,
+            givenName = json["givenName"] as? String,
+            surname = json["surname"] as? String,
+            createdAt = (json["createdAt"] as? String)?.dateFromISO8601Format,
+            modifiedAt = (json["modifiedAt"] as? String)?.dateFromISO8601Format,
+            customDataObject = json["customData"],
+            customDataObject2 = customDataObject, //For some reason we need to unwrap twice?? What?? TODO: look into why Swift does this
+            customDataData = try? NSJSONSerialization.dataWithJSONObject(customDataObject2, options: []),
+            customDataString = String(data: customDataData, encoding: NSUTF8StringEncoding) else {
+                return nil
+        }
+        
+        self.href = href
+        self.username = username
+        self.email = email
+        self.givenName = givenName
+        self.middleName = json["middleName"] as? String
+        self.surname = surname
+        self.createdAt = createdAt
+        self.modifiedAt = modifiedAt
+        self.customData = customDataString
+    }
 }
 
 internal struct UserBuilder {
@@ -54,4 +78,13 @@ internal struct UserBuilder {
     var createdAt: NSDate?
     var modifiedAt: NSDate?
     var customData: String?
+}
+
+private extension String {
+    var dateFromISO8601Format: NSDate? {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        return dateFormatter.dateFromString(self)
+    }
 }
