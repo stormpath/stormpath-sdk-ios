08bce36c8c444e249ee6357711b2481a78207504
Edward Jiang <edward@stormpath.com>
2016-03-08 14:12:33 -0800

    implements initial part of google login

--- Stormpath/Social Login/GoogleLoginProvider.swift
+++ Stormpath/Social Login/GoogleLoginProvider.swift
@@ -11,11 +11,17 @@ import Foundation
 class GoogleLoginProvider: NSObject, LoginProvider {
     var urlSchemePrefix = "com.googleusercontent.apps."
     
-    func getResponseFromCallbackURL(url: NSURL) throws -> LoginProviderResponse {
-        preconditionFailure() // TODO
+    func authenticationRequestURL(application: StormpathSocialProviderConfiguration) -> NSURL {
+        let scopes = application.scopes ?? "email profile"
+        return NSURL(string: "https://accounts.google.com/o/oauth2/auth?response_type=code&scope=\(scopes))&redirect_uri=\(application.urlScheme):/oauth2callback&client_id=\(application.appId)")!
     }
     
-    func authenticationRequestURL(application: StormpathSocialProviderConfiguration) -> NSURL {
-        preconditionFailure() // TODO
+    func getResponseFromCallbackURL(url: NSURL) throws -> LoginProviderResponse {
+        //TODO: handle error conditions
+        
+        if let authorizationCode = url.queryDictionary["code"] {
+            return LoginProviderResponse(data: authorizationCode, type: .AuthorizationCode)
+        }
+        throw StormpathError.InternalSDKError
     }
 }
